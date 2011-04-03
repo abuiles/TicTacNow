33d7fd73e636330f6e366db42fa1c55f193fd51f
Adolfo Builes <builes.adolfo@googlemail.com>
2011-04-03 16:46:06 -0500

    adding server

---
+++ server.js
@@ -0,0 +1,19 @@
+﻿var fs = require('fs');
+
+var server = require('http').createServer(function(req, response){
+  fs.readFile(__dirname+'/index.html', function(err, data){
+    response.writeHead(200, {'Content-Type':'text/html'});
+    response.write(data);
+    response.end();
+  });
+});
+server.listen(8080);
+var everyone = require("now").initialize(server);
+
+
+everyone.connected(function(){
+});
+
+
+everyone.disconnected(function(){
+});
